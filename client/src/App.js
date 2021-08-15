6131f2181904474d2c7f750b916c5de1b5579623
Konan Houphoue <bahobab@hotmail.com>
2021-08-14 23:26:56 -0500

    implemented call to all characters on app load

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+const NAME_URL = 'http://localhost:4000/characters/';
+
 function App() {
+  const [names, setNames] = useState([]);
+
+  const fetchNames = async () => {
+    const resp = await axios.get(NAME_URL);
+    setNames(resp.data);
+    // console.log(resp.data);
+  };
+
+  useEffect( () => {
+    console.log('app started');
+    fetchNames();
+  });
+
   return (
     <div className="App">
       <h1>SWAPI Exercise</h1>
+      <pre>{JSON.stringify(names)}</pre>
     </div>
   );
 }
