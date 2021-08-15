317dac34218cb3c0b9ec994003b9ad6cebb29a6a
Konan Houphoue <bahobab@hotmail.com>
2021-08-15 02:12:03 -0500

    Implemented loading state in UI

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,13 @@ const NAME_URL = 'http://localhost:4000/characters/';
 
 function App() {
   const [names, setNames] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchNames = async () => {
+    setIsLoading(true);
     const resp = await axios.get(NAME_URL);
     setNames(resp.data);
+    setIsLoading(false);
     // console.log(resp.data);
   };
 
@@ -24,25 +27,28 @@ function App() {
     <div className="App">
       <h1>SWAPI Exercise</h1>
       {/* <pre>{JSON.stringify(names)}</pre> */}
-      <SelectName names={names}/>
+      <SelectName isLoading={isLoading} names={names}/>
     </div>
   );
 }
 
-function SelectName ({names}) {
-  const [selectedValue, setSelectedValue] = useState({});
-  const [charDetails, setCharDetails] = useState({});
-
+function SelectName ({names, isLoading}) {
   const options = names.map(name => ({value: name, label: name}));
+  const [selectedValue, setSelectedValue] = useState(options[0]);
+  const [charDetails, setCharDetails] = useState({});
+  const [fetchingDetails, setFetchingDetails] = useState(false);
+
 
   const handleChange = (value) => {
     setSelectedValue(value);
   };
 
   const handleClick = async () => {
+    setFetchingDetails(true);
     const resp = await axios.get(`${NAME_URL}${selectedValue.value}`);
     // console.log(resp.data);
     setCharDetails(resp.data[0]);
+    setFetchingDetails(false);
 
   };
 
@@ -50,17 +56,25 @@ function SelectName ({names}) {
     <div>
       <h3>Select A Name to View Details</h3>
       {/* <pre>{JSON.stringify(names)}</pre> */}
-      <Select options={options} value={selectedValue} onChange={handleChange}/>
-      <button onClick={handleClick}>View Details</button>
+      <Select options={options} value={selectedValue} isLoading={isLoading} onChange={handleChange}/>
+      <button onClick={handleClick} disabled={selectedValue === undefined }>View Details</button>
       <div>
         {/* <pre>{JSON.stringify(charDetails)}</pre> */}
-        <ShowDetails characterDetails={charDetails}/>
+        {
+          fetchingDetails
+            ? "Fetching details ..."
+            : <ShowDetails characterDetails={charDetails}/>
+        }
       </div>
     </div>
   );
 }
 
 function ShowDetails({characterDetails}) {
+  if (Object.keys(characterDetails).length === 0) {
+    return 'No Details to Show Please select a Character then click the "Show Details" button'
+  }
+
   const {
     name,
     height,
